eb3b100d4203b7264973c44a7b12861238e2f2b6
heosujinnn <0204sujin@naver.com>
2023-11-12 23:57:04 +0900

    [feat] null check

--- login.php
+++ login.php
@@ -13,15 +13,27 @@ $query = "select * from moneyMember where member_email='$id' and member_pw_1='$p
 $result = mysqli_query($con, $query);
 $row = mysqli_fetch_array($result);
 
-if($id==$row['member_email'] && $pw==$row['member_pw_1']){ // id와 pw가 맞다면 login
-
-	echo "<script> alert('로그인 성공'); </script>";
-	echo "<script> location.href = 'main.html'; </script>";
-
-}else{ // id 또는 pw가 다르다면 admin_login 폼으로
-
-   echo "<script> alert('로그인 실패'); </script>";
+if (empty($id) || empty($pw)) {
+   // 아이디 또는 비밀번호가 비어있을 경우
+   echo "<script> alert('아이디와 비밀번호를 모두 입력하세요.'); </script>";
    echo "<script> location.href = 'login.html'; </script>";
+} else {
+   $query = "SELECT * FROM moneyMember WHERE member_email='$id' AND member_pw_1='$pw'";
+   $result = mysqli_query($con, $query);
+   $row = mysqli_fetch_array($result);
+
+   if ($row) {
+       // 로그인 성공
+       echo "<script> alert('로그인 성공'); </script>";
+       echo "<script> location.href = 'main.html'; </script>";
+   } else {
+       // 로그인 실패
+       echo "<script> alert('로그인 실패'); </script>";
+       echo "<script> location.href = 'login.html'; </script>";
+   }
 }
 
-?>
+
+
+
+
